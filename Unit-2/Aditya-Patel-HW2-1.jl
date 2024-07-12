3d2682f0691d4b79fb5452f4f2b4b29540a5a242
Aditya Patel <aditya.patel1616@gmail.com>
2024-07-11 23:21:14 -0500

    completed problem 2

--- Unit-2/Aditya-Patel-HW2-1.jl
+++ Unit-2/Aditya-Patel-HW2-1.jl
@@ -2,19 +2,18 @@
 using Printf
 using FixedPointNumbers
 using FixedPointDecimals
+using LinearAlgebra
+
 ##
 x = FixedDecimal{Int,2}(5)
 ## Emulate the Mario Kart system to rotate a dinosaur
 MKFP = Fixed{Int16,8} # use 16 bits for a signed integer and 8 bits for fraction
 val = MKFP(5.25)
 
-## Function to determine average square drift between 2 sets
-function avgSqDrift(act, ref)
-    if length(act) == length(ref)
-        n = length(act)
-        avgSqDrift = sum(abs2, (act - ref))/n
-    return avgSqDrift
-    end
+## Function to determine average square drift between 2 sets of equal length
+function meanEucDist(act, ref)
+    med = sqrt(sum((act .- ref).^2))
+    return med 
 end
 
 ## Get some data off the internet and plot it
@@ -24,26 +23,25 @@ data = [24,18, 24,17, 22,16, 20,11, 19,6, 19,2, 17,2, 17,6,
 9,11, 17,11, 21,17, 23,18, 24,18]
 P = reshape(data,2,div(length(data),2))
 
-x = P[1,:]
-y = P[2,:]
 using Plots
-scatter(x,y,legend=false)
-
-## Plot it in Mario Kart numbers and connect the dots
-x_mkfp = MKFP.(x)
-y_mkfp = MKFP.(y)
-plot!(x,y,legend=false) # no change!
+driftDict = Dict{Int64, Float64}()
 
 ## Rotate 359 degrees
 for deg in 1:359
-    theta = deg/180*pi
+    local plt = plot()
+    global theta = deg/180*pi
     R(theta) = [cos(theta) -sin(theta); sin(theta) cos(theta)]
-    Pp = R(theta)*P # True
-    plot(Pp[1,:],Pp[2,:],linestyle = :solid,marker = :circle,color=2,lab="Float64")
+    global Pp = R(theta)*P # True
+    plot!(plt, Pp[1,:],Pp[2,:],linestyle = :solid,marker = :circle,color=2,lab="Float64")
     # Rotate in fixed Point
-    Pmk = MKFP.(R(MKFP(theta)))*MKFP.(P)
-    plot!(Pmk[1,:],Pmk[2,:],linestyle = :solid,marker = :circle,color=4,lab="Mario Kart")
-    # Calculate point drift
-    err = avgSqDrift(Pp, Pmk)
-    @printf("Point Drift at %d degrees: %f\n", deg, err)
+    global Pmk = MKFP.(R(MKFP(theta)))*MKFP.(P)
+    plot!(plt, Pmk[1,:],Pmk[2,:],linestyle = :solid,marker = :circle,color=4,lab="Mario Kart")
+    # Calculate mean euclidean distance between points
+    local err = meanEucDist(Pp, Float64.(Pmk))
+    title!("ED between points at $deg degrees: $err")
+    push!(driftDict, deg => err)
+    display(plt)
 end
+
+## Find the maximum drift value
+maxDeg, maxDrift, = findmax(driftDict)
---
+++ Unit-2/Aditya-Patel-HW2-2.jl
@@ -0,0 +1,77 @@
+##
+using Printf
+using LinearAlgebra
+
+## Define functions for the three solution methods
+function quadraticFormula(a, b, c)
+    if b^2 > 4*a*c
+        return (-b + sqrt(b^2 - 4*a*c)) / (2*a), (-b - sqrt(b^2 - 4*a*c)) / (2*a)
+    end
+end
+
+function completeTheSquare(a, b, c)
+    ## Normalize all terms:
+    c = c/a
+    b = b/a
+    a = a/a
+
+    ## Constant term to right side:
+    c = -c
+
+    ## Add/Subtract b to make the perfect trinomial
+    b_half_sqr = (b/2)^2 
+
+    ## Squared Binomial
+    rhs = c + b_half_sqr
+
+    ## Solve for X
+    if rhs > 0
+        ## Real Roots
+        roots = -b/2 + (sqrt(rhs)), -b/2 - (sqrt(rhs))
+        return roots
+    end
+end
+
+function NewtonRaphsonMethod(a, b, c, itr, guess)
+    ## Define f(x) and f'(x)
+    f(x, a, b, c) = a*x^2 + b*x + c
+    f_prime(x, a, b) = 2*a*x + b
+
+    x = guess
+    for n in 1:itr
+        f_x = f(x, a, b, c)
+        f_xp = f_prime(x, a, b)
+        if abs(f_xp) < 1e-10
+            return x
+        end
+        x_new = x - f_x/f_xp
+        if abs(x_new - x) < 1e-10
+            return x_new
+        end
+        x = x_new
+    end
+    return x
+end
+
+## Execute with Float16 values for a, b, c, and x_guess
+
+for i in 0:10
+    for j in 0:10
+        for k in 0:10
+            local a = Float64(i)
+            local b = Float64(j)
+            local c = Float64(k)
+            local x_guess = Float64((sum([i, j, k])/3))
+
+            quadRoot = quadraticFormula(a, b, c)
+            ctsRoot = completeTheSquare(a, b, c)
+            newtonRoot = NewtonRaphsonMethod(a, b, c, 1000, x_guess)
+
+            if  !isnothing(quadRoot) && !isnothing(ctsRoot) ## Only take real zeros
+                if quadRoot[1] != ctsRoot[1] != newtonRoot != quadRoot[1] ## Check for where they are not equal
+                    println("Root 1 with a=$i, b=$j, c=$k:\t", quadRoot[1], "\t\t\t", ctsRoot[1], "\t\t\t", newtonRoot)
+                end
+            end
+        end
+    end
+end
